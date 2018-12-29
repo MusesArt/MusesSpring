bf21c8141aebd82067ddff0bd967e7f673d088d6
Victor Xu <VanXNF@gmail.com>
2018-12-29 23:07:50 +0800

    1. add PageModel & StatusModel 2. update BaseDaoImpl

--- src/main/java/muses/art/controller/CommodityController.java
+++ src/main/java/muses/art/controller/CommodityController.java
@@ -1,6 +1,8 @@
 package muses.art.controller;
 
 import muses.art.entity.commodity.Commodity;
+import muses.art.model.PageModel;
+import muses.art.model.StatusModel;
 import muses.art.service.commodity.CommodityService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -9,6 +11,8 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpSession;
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 @RequestMapping("api/commodity")
@@ -18,9 +22,26 @@ public class CommodityController {
     private CommodityService commodityService;
 
     @RequestMapping(value = "/id", method = RequestMethod.GET)
-    public @ResponseBody Commodity get(HttpSession session) {
-        Commodity commodity = new Commodity();
-        commodity.setId(1234124);
-        return commodity;
+    public @ResponseBody StatusModel<PageModel<Commodity>> get(HttpSession session) {
+
+        List<Commodity> commodities = new ArrayList<>();
+        for (int i = 0; i < 10; i++ ) {
+            Commodity commodity = new Commodity();
+            commodity.setId(i);
+            commodities.add(commodity);
+        }
+        PageModel<Commodity> pageModel = new PageModel<>();
+        pageModel.setCurrentPage(1);
+        pageModel.setDataList(commodities);
+        pageModel.setPageCount(1);
+        pageModel.setPageSize(10);
+        pageModel.setTotalNum(10);
+        StatusModel<PageModel<Commodity>> statusModel = new StatusModel<>();
+        statusModel.setErrorCode(0);
+        statusModel.setErrorMsg("ERROR CODE == 0");
+        PageModel<Commodity> commodityPageModel = new PageModel<>();
+        commodityPageModel.setDataList(new ArrayList<Commodity>());
+        statusModel.setData(commodityPageModel);
+        return statusModel;
     }
 }
