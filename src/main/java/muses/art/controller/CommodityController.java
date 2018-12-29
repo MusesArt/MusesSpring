ec02c39ab9149d905efb0719de95e60fe76bf20a
Victor Xu <VanXNF@gmail.com>
2018-12-29 19:52:14 +0800

    fix Deployment Descriptors bug in web.xml

--- src/main/java/muses/art/controller/CommodityController.java
+++ src/main/java/muses/art/controller/CommodityController.java
@@ -1,10 +1,12 @@
 package muses.art.controller;
 
+import muses.art.entity.commodity.Commodity;
 import muses.art.service.commodity.CommodityService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpSession;
 
@@ -15,8 +17,10 @@ public class CommodityController {
     @Autowired
     private CommodityService commodityService;
 
-    @RequestMapping(value = "/", method = RequestMethod.GET)
-    public String get(HttpSession session) {
-        return "redirect:/";
+    @RequestMapping(value = "/id", method = RequestMethod.GET)
+    public @ResponseBody Commodity get(HttpSession session) {
+        Commodity commodity = new Commodity();
+        commodity.setId(1234124);
+        return commodity;
     }
 }
