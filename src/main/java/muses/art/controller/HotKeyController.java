7d9953cdfd766f6406cef5f8624b763d35c1a519
Victor Xu <VanXNF@gmail.com>
2019-01-04 16:29:29 +0800

    fix CORS bug in HotKey API

--- src/main/java/muses/art/controller/HotKeyController.java
+++ src/main/java/muses/art/controller/HotKeyController.java
@@ -8,6 +8,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -22,6 +23,7 @@ public class HotKeyController {
     private HotKeyService hotKeyService;
     private Logger logger = LoggerFactory.getLogger(this.getClass());
 
+    @CrossOrigin(origins = "*", maxAge = 3600)
     @RequestMapping(value = "/", method = RequestMethod.GET)
     public @ResponseBody StatusModel<List<HotKeyModel>> findHotKeyList() {
         List<HotKeyModel> hotKeyModels = hotKeyService.findHotKeyList();
