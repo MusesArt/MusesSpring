ddb20b32ba7d05298d8242afdcc154f6f626850c
czczcz <wztxy89@63.xn--2hvr00d>
2019-01-07 03:22:12 +0800

    新增返回用户头像和用户名

--- src/main/java/muses/art/model/user/TokenModel.java
+++ src/main/java/muses/art/model/user/TokenModel.java
@@ -3,6 +3,24 @@ package muses.art.model.user;
 public class TokenModel {
     private String token;
     private Integer userId;
+    private String username;
+    private String avatar;
+
+    public String getAvatar() {
+        return avatar;
+    }
+
+    public void setAvatar(String avatar) {
+        this.avatar = avatar;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
 
     public Integer getUserId() {
         return userId;
